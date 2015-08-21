8779797527676ebae20571d551830518bac8650c
Dany Haddad <danyhaddad43@gmail.com>
2015-08-21 00:12:11 -0400

    Changed documentation to describe a system which uses a netlist

--- Conversion.py
+++ Conversion.py
@@ -1,3 +1,23 @@
 __author__ = 'Dany'
+""" Each circuit is represented by a netlist where each line has the following format:
+        [component refdes] [list of nodes to connect to] [value]
+    Component Types:
+        -Resistor 'R'
+        -Capacitor 'C'
+        -Inductor 'L'
+        -Complex Impedance 'Z'
+        -Voltage source 'V'
+        -Current source 'I'
+        -Voltage Controlled voltage source 'VCVS'
+        -Current controlled voltage source 'CCVS'
+        -Voltage controlled current source 'VCIS'
+        -Current controlled current source 'CCIS'
+    Default units are:
+        -Impedance: Ohms
+        -Voltage source: V
+        -Current Source: A
+    Each circuit has a corresponding admittance matrix and current injection vector:
+        I = YV; where I and V are vectors and Y is a matrix
+"""
 
 def
